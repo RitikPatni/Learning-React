1193e2005d1075ae21a328cf3c5ea0c633bdb5ee
Ritik <ritik.patni@epaylater.in>
2021-07-20 18:21:24 +0530

    feat: Day 1: Binary to Decimal

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Bin2Dec from './Components/Bin2Dec'
+let timer = undefined
+function App() {
+  return (
+    <div className="App">
+      <Bin2Dec />
+    </div>
+  )
+}
+
+export default App
